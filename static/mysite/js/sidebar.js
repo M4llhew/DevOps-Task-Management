01b5994aa96d6292f7a6d70c4fc2f7ac18517d99
M4llhew <noblematrich@gmail.com>
2024-05-09 18:19:17 +0100

    changing js to implement through css for sidebar

--- static/mysite/js/sidebar.js
+++ static/mysite/js/sidebar.js
@@ -1,29 +1,13 @@
-
-
-function toggleSidebar() {
-    var toggleVisuallyHidden = document.getElementsByClassName('needs-hiding');
-    for(var i = 0; i < toggleVisuallyHidden.length; i++) {
-    // Do something with each element, for example, hide it
-    toggleVisuallyHidden[i].classList.toggle('visually-hidden')
-    }
-
-    const sidebarContent = document.querySelector('.sidebar-content');
-    const sidebarContainer = document.querySelector('.sidebar-container');
-
-    console.log('Toggle Sidebar called'); // Check if this log appears in the console
-    console.log("clicked");
-//    expandedSidebar.classList.toggle('hide');
-//    collapsedSidebar.classList.toggle('hide');
-    sidebarContainer.classList.toggle('collapsed-size');
-    sidebarContent.classList.toggle('sidebar-collapsed');
-    sidebarContent.classList.toggle('sidebar-expanded');
-}
-
-
 document.addEventListener('DOMContentLoaded', function() {
+    const sidebarContent = document.querySelector('.sidebar-content');
     const openButton = document.querySelector('.sidebar-closed-button');
     const closeButton = document.querySelector('.sidebar-opened-button');
-    console.log(openButton, closeButton, "Test")
+
+    function toggleSidebar() {
+        sidebarContent.classList.toggle('sidebar-collapsed');
+        sidebarContent.classList.toggle('sidebar-expanded');
+    }
+
     openButton.addEventListener('click', toggleSidebar);
     openButton.addEventListener('touchstart', toggleSidebar);
     closeButton.addEventListener('click', toggleSidebar);
